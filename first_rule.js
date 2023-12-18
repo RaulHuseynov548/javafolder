a0540874a308725a3a30202456689a52e79f8823
RaulHuseynov548 <raulhuseynov548@gmail.com>
2023-12-18 14:37:44 +0400

    test commit

---
+++ first_rule.js
@@ -0,0 +1,17 @@
+alert ("wellcome")
+var your_name = prompt ("Adinizi daxil edin");
+console.log (`Adınız ${your_name}`)
+var your_order =prompt ("Sİfaris nomreniz");
+console.log (`Sifaris: ${your_order} burger,cola`)
+
+if  (your_order  <=5){
+    alert (`${your_order} kassaya odenis edin` )
+
+}
+
+else { (your_order  > 5) 
+    alert (`${your_order} 5'e qeder sifaris ede bilersiniz`)
+   }
+
+
+
